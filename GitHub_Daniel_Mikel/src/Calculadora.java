2af5eabd51713922b2a81c08b0d6c20b921338fc
Diurno <Diurno@DAM124>
2026-03-11 09:25:51 +0100

    Commit inicial: Suma y Resta

--- GitHub_Daniel_Mikel/src/Calculadora.java
+++ GitHub_Daniel_Mikel/src/Calculadora.java
@@ -11,5 +11,5 @@ public class Calculadora {
 		Calculadora calc = new Calculadora();
 		System.out.println("--- INICIO DE LA CALCULADORA ---");
 		System.out.println("Suma: " + calc.sumar(10, 5));
-	}
+	} 
 }
